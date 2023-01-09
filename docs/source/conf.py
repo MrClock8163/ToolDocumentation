2fde34ffacb7821d144c2c9f60856dcbb073d793
MrClock <48679831+MrClock8163@users.noreply.github.com>
2023-01-09 12:56:52 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MrClock''s modding tools'
+copyright = '2023, MrClock'
+author = 'MrClock'
 
 release = '0.1'
 version = '0.1.0'

87e4cfce5e4d42e7583e2d47cd50be89e0f0a280
MrClock8163 <jz.zalan@gmail.com>
2023-01-10 21:36:58 +0100

    Config changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = "MrClock's modding tools"
 copyright = '2023, MrClock'
 author = 'MrClock'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0'
 
 # -- General configuration
 
